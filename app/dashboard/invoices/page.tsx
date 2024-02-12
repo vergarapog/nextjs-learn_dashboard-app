b42a51a0584df20cb64b245ce5dc881c10aef048
Brian <vbrian.dev@gmail.com>
2024-02-12 10:41:34 +0800

    finish up to chap 5

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return <div>Invoices Page</div>;
+};
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return <div>Dashboard page</div>;
+};
+
+export default Page;
