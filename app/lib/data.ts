41d33e0653fbf4f64ed5d59aaa96ef1214bed16a
vergarapog <vbrian.dev@gmail.com>
2024-09-04 15:10:13 +0800

    finished chapter 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -39,7 +39,7 @@ export async function fetchLatestInvoices() {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
+      JOIN customers ON invoices.customer_id = customers.id'
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
